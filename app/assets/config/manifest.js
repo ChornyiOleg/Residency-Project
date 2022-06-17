2a59d8d46e18c31346de6011fe64ef25e6e10c8b
ChornyiOleg <olegchornyi82@gmail.com>
2022-06-17 08:16:04 +0300

    assets:precompile fix

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,7 @@
 //= link_tree ../images
 //= link_tree ../builds
 //= link_tree ../stylesheets
-//= link_tree ../javascripts
-//= link_tree ../webfonts
+// link_tree ../javascripts
+// link_tree ../../vendor/fonts
+// link_tree ../../vendor/webfonts
 
