00861f4c0f61223f64cb96aee424d281c78db2b8
Anton921 <antonshemiakin@gmail.com>
2022-06-21 17:41:25 +0300

    update_residence.rb

--- app/models/residence.rb
+++ app/models/residence.rb
@@ -2,7 +2,7 @@ class Residence < ApplicationRecord
   belongs_to :country
   belongs_to :program
   has_many :orders, dependent: :destroy
-  has_many :likes, dependent: :destroys
+  has_many :likes, dependent: :destroy
 
   has_many_attached :images
 end
