a45ad75d9a3cc59e2636d4c8e1a356a9234a77e2
Anton921 <antonshemiakin@gmail.com>
2022-06-21 18:19:01 +0300

    order_validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,7 @@ class Order < ApplicationRecord
   belongs_to :user
 
   validates :name, presence: true, length: { minimum: 2, maximum: 100 }
-  validates :phone, presence: true, format:{ :with /^[0-9-()+]{6,16}$/ }
+  validates :phone, presence: true
   validates :info, presence: true, length: { minimum: 2, maximum: 100 }
 
   enum status: %i[new_request completed]
