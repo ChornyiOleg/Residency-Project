072a7119668559f68f9998467781825989429b28
ChornyiOleg <olegchornyi82@gmail.com>
2022-06-29 03:10:24 +0300

    update Testimonials, Programs, Posts on the Home page

--- app/models/testimonial.rb
+++ app/models/testimonial.rb
@@ -1,4 +1,4 @@
 class Testimonial < ApplicationRecord
   validates :name_of_user, presence: true
-  validates :content, presence: true, length: { minimum: 2, maximum: 100 }
+  validates :content, presence: true, length: { minimum: 2, maximum: 500 }
 end
