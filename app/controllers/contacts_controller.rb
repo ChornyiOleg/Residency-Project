e80aab3b479c4b2c9cf6310cb83b6856884e4131
Anton921 <antonshemiakin@gmail.com>
2022-07-01 18:36:38 +0300

    last

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,59 +1,8 @@
 class ContactsController < ApplicationController
-  before_action :set_contact, only: %i[edit update show destroy]
-
   def index
     @contacts = Contact.all
     @pagy, @contacts = pagy(Contact.order(created_at: :desc), items: 5)
   end
 
   def show; end
-
-  def new
-    @contact = Contact.new
-  end
-
-  def edit; end
-
-  def create
-    @contact = Contact.new(contact_params)
-    respond_to do |format|
-      if @contact.save
-        format.html { redirect_to @contact, notice: 'Contact was successfully created.' }
-        format.json { render :show, status: :created, location: @contact }
-      else
-        format.html { render :new }
-        format.json { render json: @contact.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def update
-    respond_to do |format|
-      if @contact.update(post_params)
-        format.html { redirect_to @contact, notice: 'Contact was successfully updated.' }
-        format.json { render :show, status: :ok, location: @contact }
-      else
-        format.html { render :edit }
-        format.json { render json: @contact.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def destroy
-    @contact.destroy
-    respond_to do |format|
-      format.html { redirect_to contacts_url, notice: 'Contact was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-
-  def set_contact
-    @contact = Contact.find(params[:id])
-  end
-
-  def contact_params
-    params.require(:contact).permit(:location, :email, :phone)
-  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,59 +1,8 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: %i[edit update show destroy]
-
   def index
     @posts = Post.all
     @pagy, @posts = pagy(Post.order(created_at: :desc), items: 5)
   end
 
   def show; end
-
-  def new
-    @post = Post.new
-  end
-
-  def edit; end
-
-  def create
-    @post = Post.new(post_params)
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { render :show, status: :ok, location: @post }
-      else
-        format.html { render :edit }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def destroy
-    @post.destroy
-    respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-
-  def set_post
-    @post = Post.find(params[:id])
-  end
-
-  def post_params
-    params.require(:post).permit(:name, :content, :image)
-  end
 end
